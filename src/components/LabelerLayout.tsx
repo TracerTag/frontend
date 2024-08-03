382a7e609b628730a6a6e03ab81143df364ad603
Giacomo Ferretti <giacomo.ferretti.00@gmail.com>
2024-08-03 06:50:07 +0200

    diamond_shovel

--- src/components/LabelerLayout.tsx
+++ src/components/LabelerLayout.tsx
@@ -1,6 +1,11 @@
 import { useRef } from "react";
 import dynamic from "next/dynamic";
-import { Trash2Icon } from "lucide-react";
+import {
+  DownloadIcon,
+  SparklesIcon,
+  Trash2Icon,
+  Wand2Icon,
+} from "lucide-react";
 
 import { Button } from "~/components/ui/button";
 import { useAppStore } from "~/store/app-store";
@@ -8,6 +13,18 @@ import { useAppStore } from "~/store/app-store";
 const Canvas = dynamic(() => import("./Canvas").then((m) => m.Canvas), {
   ssr: false,
 });
+
+const LabelEntry = ({ label }: { label: string }) => {
+  return (
+    <div className="flex rounded border p-4">
+      <img
+        src="https://via.placeholder.com/40"
+        className="mr-4 aspect-square"
+      />
+      <span className="font-bold">{label}</span>
+    </div>
+  );
+};
 
 const SideBar = () => {
   const imageUrl = useAppStore((s) => s.imageUrl);
@@ -19,23 +36,52 @@ const SideBar = () => {
   };
 
   return (
-    <div className="p-4">
-      <Button className="w-full" onClick={removeImage}>
-        <Trash2Icon className="mr-2 h-5 w-5" /> Remove image
-      </Button>
-    </div>
+    <ol role="list" className="flex flex-1 flex-col gap-y-7 p-4">
+      <li className="h-72 space-y-4 overflow-y-auto">
+        {Array.from({ length: 20 }).map((_, i) => (
+          <LabelEntry key={i} label={`Label ${i}`} />
+        ))}
+      </li>
+      <li className="mt-auto">
+        <ul role="list" className="space-y-2">
+          <li>
+            <Button
+              variant="secondary"
+              className="w-full"
+              onClick={removeImage}>
+              <Trash2Icon className="mr-2 h-5 w-5" /> Remove image
+            </Button>
+          </li>
+          <li>
+            <Button className="w-full">
+              <Wand2Icon className="mr-2 h-5 w-5" /> Extract outlines
+            </Button>
+          </li>
+          <li className="flex gap-2">
+            <Button className="flex-1">
+              <DownloadIcon className="mr-2 h-5 w-5" /> SVG
+            </Button>
+            <Button className="flex-1">
+              <DownloadIcon className="mr-2 h-5 w-5" /> JSON
+            </Button>
+          </li>
+        </ul>
+      </li>
+    </ol>
   );
 };
 
 export const LabelerLayout = () => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const imageUrl = useAppStore((s) => s.imageUrl);
 
   return (
     <div className="flex flex-1 items-stretch justify-center">
       {/* Main */}
       <div className="relative flex flex-1 flex-col items-center justify-center overflow-visible px-12 py-9">
         <div className="relative h-auto w-full" ref={containerRef}>
-          <Canvas containerRef={containerRef} />
+          {/* <Canvas containerRef={containerRef} /> */}
+          <img src={imageUrl} />
         </div>
       </div>
 
